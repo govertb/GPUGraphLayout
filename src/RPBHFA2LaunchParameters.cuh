fbcc8ded82ec13838d6c7c7918129a8c6f307c09
Govert <ggbrinkmann@gmail.com>
2018-01-13 13:00:26 +0100

    Fix Execution Configuration if __CUDA_ARCH__ >= 300

--- src/RPBHFA2LaunchParameters.cuh
+++ src/RPBHFA2LaunchParameters.cuh
@@ -9,36 +9,36 @@
 
 #if __CUDA_ARCH__ >= 500 // Maxwell (5.x) or Pascal (6.x)
 
-#define THREADS1 1024  /* must be a power of 2 */
-#define THREADS2 1024
-#define THREADS3 768
-#define THREADS4 128
-#define THREADS5 1024
+#define THREADS1 512  /* must be a power of 2 */
+#define THREADS2 512
+#define THREADS3 128
+#define THREADS4 64
+#define THREADS5 256
 #define THREADS6 1024
 
-#define FACTOR1 2
-#define FACTOR2 2
-#define FACTOR3 1  /* must all be resident at the same time */
-#define FACTOR4 4  /* must all be resident at the same time */
-#define FACTOR5 2
-#define FACTOR6 2
+#define FACTOR1 3
+#define FACTOR2 3
+#define FACTOR3 6  /* must all be resident at the same time */
+#define FACTOR4 6  /* must all be resident at the same time */
+#define FACTOR5 5
+#define FACTOR6 1
 
 
 #elif __CUDA_ARCH__ >= 300 // Kepler (3.x)
 
-#define THREADS1 1024  /* must be a power of 2 */
-#define THREADS2 1024
-#define THREADS3 768
-#define THREADS4 128
-#define THREADS5 1024
+#define THREADS1 512  /* must be a power of 2 */
+#define THREADS2 512
+#define THREADS3 128
+#define THREADS4 64
+#define THREADS5 256
 #define THREADS6 1024
 
-#define FACTOR1 2
-#define FACTOR2 2
-#define FACTOR3 1  /* must all be resident at the same time */
-#define FACTOR4 4  /* must all be resident at the same time */
-#define FACTOR5 2
-#define FACTOR6 2
+#define FACTOR1 3
+#define FACTOR2 3
+#define FACTOR3 6  /* must all be resident at the same time */
+#define FACTOR4 6  /* must all be resident at the same time */
+#define FACTOR5 5
+#define FACTOR6 1
 
 #elif __CUDA_ARCH__ < 300 // Fermi (2.x) or Tesla (1.x)
 
